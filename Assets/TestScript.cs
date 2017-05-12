753c261b8abde265f3ed42095dbb635c9e365c75
ConsciousObserver <ConsciousObserver@nomail.com>
2017-05-11 23:48:51 -0400

    Removed unity default comments

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class TestScript : MonoBehaviour {
 
     public Script1 script1;
-	// Use this for initialization
+	
 	void Start () {
         script1 = new Script1(123);
         Debug.Log("count = " + script1.getCount());
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
