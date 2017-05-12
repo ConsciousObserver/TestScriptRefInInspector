8eed51c04f2981bd402195e6db0523166d3fae82
ConsciousObserver <ConsciousObserver@nomail.com>
2017-05-11 23:44:20 -0400

    Added code to show that instances of Monobehavior child can be created created with new, this only results in warning

---
+++ Assets/Script1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour {
+
+    int count;
+
+    public Script1()
+    {
+        /*empty constructor*/
+    }
+    public Script1(int count)
+    {
+        this.count = count;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+    public Script1 script1;
+	// Use this for initialization
+	void Start () {
+        script1 = new Script1(123);
+        Debug.Log("count = " + script1.getCount());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
